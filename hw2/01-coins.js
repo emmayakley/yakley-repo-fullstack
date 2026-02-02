9035a7bd8c5cc4a759e3c6af14d793d2478db847
emmayakley <emmie.yakley@gmail.com>
2026-02-02 14:10:35 -0800

    implemented changes and finished exercise 4.

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -6,32 +6,31 @@ const calculateChange = function convertDollarAmountToCoinEquivalent(
   dollarAmount
 ) {
   if (dollarAmount > 100) {
-    console.error("The input amount must be $100 or less");
+    console.error('The input amount must be $100 or less');
     return;
   }
 
   let total = dollarAmount;
 
   const dollars = Math.floor(dollarAmount / 1);
-  console.log("Dollars:", dollars);
+  console.log('Dollars:', dollars);
   total = total - dollars;
 
   const quarters = Math.floor(total / 0.25);
-  console.log("Quarters:", quarters);
+  console.log('Quarters:', quarters);
   total = total - quarters * 0.25;
 
   const dimes = Math.floor(total / 0.1);
-  console.log("Dimes:", dimes);
+  console.log('Dimes:', dimes);
   total = total - dimes * 0.1;
 
   const nickels = Math.floor(total / 0.05);
-  console.log("Nickels:", nickels);
+  console.log('Nickels:', nickels);
   total = total - nickels * 0.05;
 
   const pennies = Math.floor(total / 0.01);
-  console.log("Pennies:", pennies);
+  console.log('Pennies:', pennies);
   total = total - pennies * 0.01;
-  
 };
 
 // Sample test cases
